a3a961664d60a8fdc24df57517ebb0a14f08996c
Laughing <61612323+Laughing-q@users.noreply.github.com>
2020-04-08 15:11:31 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,7 +5,6 @@ from PIL import Image
 from torch.utils.data import DataLoader
 from torchvision import datasets
 import numpy as np
-import pandas as pd
 import os
 
 workers = 0 if os.name == 'nt' else 4
